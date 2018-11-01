e6fae1e24a7fef1e304a5f1ad5a35dea4e52d6ef
Juho Huhtanen <juhohuh@ety-a2123b-29.edu.wks.metropolia.fi>
2018-11-01 09:59:14 +0200

    tehtäviä

---
+++ js5/js/teht1.js
@@ -0,0 +1,23 @@
+document.getElementsByTagName('td')[0].innerHTML = 'Juho';
+document.getElementsByTagName('td')[1].innerHTML = 'Huhtanen';
+document.getElementsByTagName('td')[2].innerHTML = 'Juhislol';
+document.getElementsByTagName('td')[3].innerHTML = 'Mannerheimintie 200';
+
+const tr1 = document.querySelector('tr:first-child');
+const th = document.createElement('th');
+const p = document.createTextNode('Puhelinnumero');
+th.appendChild(p);
+tr1.appendChild(th);
+const tr2 = document.querySelector('tr:last-child');
+const td = document.createElement('td');
+const p2 = document.createTextNode('4063464');
+td.appendChild(p2);
+tr2.appendChild(td);
+
+let parent = document.getElementsByTagName('tr');
+let node = document.getElementsByTagName('th')[2].innerHTML = '';
+parent.removeChild(node);
+
+let parent2 = document.getElementsByTagName('tr');
+let node2 = document.getElementsByTagName('td')[2].innerHTML = '';
+parent2.removeChild(node2);
