4162a860bb9036c4e207447429c53a6dd582b271
Juho Huhtanen <juhohuh@ety-a2123b-29.edu.wks.metropolia.fi>
2018-11-07 12:13:58 +0200

    tehtävien palautusta

--- js7/js/teht3.js
+++ js7/js/teht3.js
@@ -1,16 +1,8 @@
-document.getElementById('sum').click = function() {
-  sumClick();
-};
+document.getElementById('sum').addEventListener('click', sumClick);
+document.getElementById('sub').addEventListener('click', subClick);
+document.getElementById('multi').addEventListener('click', multiClick);
+document.getElementById('div').addEventListener('click', divClick);
 
-function sumClick() {
-  let nro1 = function() {
-    getNmbr1();
-  };
-  let nro2 = function() {
-    getNmbr2();
-  };
-  document.getElementById('vastaus').innerHTML = nro1 + nro2;
-}
 
 function getNmbr1() {
   return document.getElementById('num1').value;
@@ -19,3 +11,28 @@ function getNmbr1() {
 function getNmbr2() {
   return document.getElementById('num2').value;
 }
+
+function sumClick() {
+  let nro1 = Number(getNmbr1());
+  let nro2 = Number(getNmbr2());
+  document.getElementById('vastaus').innerHTML = nro1 + nro2;
+}
+
+function subClick() {
+  let nro1 = Number(getNmbr1());
+  let nro2 = Number(getNmbr2());
+  document.getElementById('vastaus').innerHTML = nro1 - nro2;
+}
+
+function multiClick() {
+  let nro1 = Number(getNmbr1());
+  let nro2 = Number(getNmbr2());
+  document.getElementById('vastaus').innerHTML = nro1 * nro2;
+}
+
+function divClick() {
+  let nro1 = Number(getNmbr1());
+  let nro2 = Number(getNmbr2());
+  document.getElementById('vastaus').innerHTML = nro1 / nro2;
+}
+
