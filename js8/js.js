2392816aad6b3674687fc258b916d70c63a96057
Juho Huhtanen <juhohuh@ety-a2123b-29.edu.wks.metropolia.fi>
2018-11-08 12:13:29 +0200

    ajax tehtävää

---
+++ js8/js.js
@@ -0,0 +1,25 @@
+document.getElementById('hakunappi').addEventListener('click', search, false);
+
+function search() {
+  let userInput = document.getElementById('hakukentta').value;
+  fetch('http://api.tvmaze.com/search/shows?q=' + userInput).
+      then(response => response.json()).
+      then(shows => {
+        const sarjat = document.getElementById('sarjat');
+        sarjat.innerHTML = shows.map(({show}) => `
+          <div class="uudet">
+            ${show.image ? `<img src="${show.image.medium}">` : ''}
+            <div>
+              <h3>${show.name}</h3>
+              <span><a href="${show.officialSite}">Open show homepage</a></span>
+              <br />
+              <span>Summary: ${show.summary}</span>
+            </div>
+          </div>
+        `).join('');
+      }).
+      catch(error => {
+        console.log(error);
+      });
+  return;
+}
